c1f626f8a0cbf59a3b2ce5f5ebdff2f703db7309
Rahul Kumar Sahu <124787795+RahulSahu16@users.noreply.github.com>
2026-02-17 22:29:17 +0530

    Fix syntax error in fetchAll callback

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,7 +1,7 @@
 const Home  = require("../models/home");
 
 exports.getHomePage = (req, res, next) => {
-  Home.fetchAll(registeredHomes => {;
+  Home.fetchAll(registeredHomes => {
   res.render("store/homepage", {registeredHomes: registeredHomes,pageTitle: "Welcome to Stay Cozy"});
 })
 } 
