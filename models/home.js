6c425f451a8e68166f7de7608a62abaa4a915e2a
Rahul Kumar Sahu <rahulssahu1116@gmail.com>
2026-02-17 21:06:35 +0530

    Try to optimize and create data folder to save homes

--- models/home.js
+++ models/home.js
@@ -1,10 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path-util');
+const { error } = require('console');
+const homeFilePath = path.join(rootDir, 'data', 'homes.json');
+
 const registeredHomes = [];
 
 module.exports = class Home{
-    constructor(houseName){
+    constructor(houseName, city, price, rating, imageURL, description){
         this.houseName = houseName;
+        this.city = city;
+        this.price = price;
+        this.rating = rating;
+        this.imageURL = imageURL;
+        this.description = description;
     }
-    save(){
-        registeredHomes.push(this);
+    save(callback){
+        
+        Home.fetchAll(registeredHomes => registeredHomes.push(this));
+        fs.writeFile(homeFilePath, JSON.stringify(registeredHomes), callback)
+    }
+    static fetchAll(callback){
+        fs.readFile(homeFilePath, (error, data) => {
+            let homes = [];
+            if(error){
+                callback([]);
+            }else{
+            callback(JSON.parse(data));
+            }
+        });
+
     }
 }
